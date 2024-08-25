f2c60d8073d21d885f9be8bb3441af8c095b8f83
Kamilek <radek71k@onet.pl>
2024-08-25 11:46:44 +0200

    Application user, migration

--- OnlineCourses/Data/ApplicationDbContext.cs
+++ OnlineCourses/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using OnlineCourses.Entities;
 
 namespace OnlineCourses.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
