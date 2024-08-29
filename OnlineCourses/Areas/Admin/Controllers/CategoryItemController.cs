105c4c04aac23f6e7c2ba9ca8d77716e8a16888f
Kamilek <radek71k@onet.pl>
2024-08-29 14:34:05 +0200

    Roles and Authorization added

--- OnlineCourses/Areas/Admin/Controllers/CategoryItemController.cs
+++ OnlineCourses/Areas/Admin/Controllers/CategoryItemController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -14,6 +15,7 @@ using OnlineCourses.Repositories;
 namespace OnlineCourses.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles = "Admin")]
     public class CategoryItemController : Controller
     {
         private readonly ICategoryItemRepository _categoryItemRepository;
--- OnlineCourses/Areas/Admin/Controllers/MediaTypeController.cs
+++ OnlineCourses/Areas/Admin/Controllers/MediaTypeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -13,6 +14,7 @@ using OnlineCourses.Repositories;
 namespace OnlineCourses.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles = "Admin")]
     public class MediaTypeController : Controller
     {
         private readonly IMediaTypeRepository _mediaTypeRepository;
