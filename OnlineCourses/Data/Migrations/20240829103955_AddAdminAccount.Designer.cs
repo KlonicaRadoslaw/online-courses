a40e04d73075dce8f307b2b63078f71261b4c0eb
Kamilek <radek71k@onet.pl>
2024-08-29 14:19:19 +0200

    Custom migration adding Admin to database

--- OnlineCourses/Data/Migrations/20240829103955_AddAdminAccount.Designer.cs
+++ OnlineCourses/Data/Migrations/20240829103955_AddAdminAccount.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OnlineCourses.Data;
 
@@ -11,9 +12,10 @@ using OnlineCourses.Data;
 namespace OnlineCourses.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240829103955_AddAdminAccount")]
+    partial class AddAdminAccount
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ OnlineCourses/Data/Migrations/20240829103955_AddAdminAccount.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Migrations;
+using OnlineCourses.Entities;
+using System.Text;
+
+#nullable disable
+
+namespace OnlineCourses.Data.Migrations
+{
+    public partial class AddAdminAccount : Migration
+    {
+        string ADMIN_USER_GUID = Guid.NewGuid().ToString();
+        string ADMIN_ROLE_GUID = Guid.NewGuid().ToString();
+
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var hasher = new PasswordHasher<ApplicationUser>();
+
+            var passwordHash = hasher.HashPassword(null, "Password100!");
+
+            var sb = new StringBuilder();
+
+            sb.AppendLine("INSERT INTO AspNetUsers(Id, UserName, NormalizedUserName,Email,EmailConfirmed,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnabled,AccessFailedCount,NormalizedEmail,PasswordHash,SecurityStamp,FirstName,PostCode,Address1,Address2,LastName)");
+            sb.AppendLine("VALUES(");
+            sb.AppendLine($"'{ADMIN_USER_GUID}'");
+            sb.AppendLine(",'admin@gmail.com'");
+            sb.AppendLine(",'ADMIN@GMAIL.COM'");
+            sb.AppendLine(",'admin@gmail.com'");
+            sb.AppendLine(", 0");
+            sb.AppendLine(", 0");
+            sb.AppendLine(", 0");
+            sb.AppendLine(", 0");
+            sb.AppendLine(", 0");
+            sb.AppendLine(",'ADMIN@GMAIL.COM'");
+            sb.AppendLine($", '{passwordHash}'");
+            sb.AppendLine(", ''");
+            sb.AppendLine(",'Admin'");
+            sb.AppendLine(",'00-000'");
+            sb.AppendLine(",''");
+            sb.AppendLine(",''");
+            sb.AppendLine(",'Admin'");
+            sb.AppendLine(")");
+
+            migrationBuilder.Sql(sb.ToString());
+
+            migrationBuilder.Sql($"INSERT INTO AspNetRoles (Id, Name, NormalizedName) VALUES ('{ADMIN_ROLE_GUID}','Admin','ADMIN')");
+
+            migrationBuilder.Sql($"INSERT INTO AspNetUserRoles (UserId, RoleId) VALUES ('{ADMIN_USER_GUID}','{ADMIN_ROLE_GUID}')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($"DELETE FROM AspNetUserRoles WHERE UserId = '{ADMIN_USER_GUID}' AND RoleId = '{ADMIN_ROLE_GUID}'");
+
+            migrationBuilder.Sql($"DELETE FROM AspNetUsers WHERE Id = '{ADMIN_USER_GUID}'");
+
+            migrationBuilder.Sql($"DELETE FROM AspNetRoles WHERE Id = '{ADMIN_ROLE_GUID}'");
+        }
+    }
+}
