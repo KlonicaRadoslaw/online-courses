9db50a030dc4c734ad26ce0f769042605e89a398
Kamilek <radek71k@onet.pl>
2024-08-30 16:13:24 +0200

    Disabling Register button when checkbox is unchecked

--- OnlineCourses/wwwroot/js/UserRegistration.js
+++ OnlineCourses/wwwroot/js/UserRegistration.js
@@ -15,6 +15,36 @@
 
     }
 
+    $("#UserRegistrationModal input[name = 'Email']").blur(function () {
+
+        var email = $("#UserRegistrationModal input[name = 'Email']").val();
+
+        var url = "UserAuth/UserNameExists?userName=" + email;
+
+        $.ajax({
+            type: "GET",
+            url: url,
+            success: function (data) {
+                if (data == true) {
+
+                    PresentClosableBootstrapAlert("#alert_placeholder_register", "warning", "Invalid Email", "This email address has already been registered");
+
+                }
+                else {
+                    CloseAlert("#alert_placeholder_register");
+                }
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                var errorText = "Status: " + xhr.status + " - " + xhr.statusText;
+
+                PresentClosableBootstrapAlert("#alert_placeholder_register", "danger", "Error!", errorText);
+
+                console.error(thrownError + '\r\n' + xhr.statusText + '\r\n' + xhr.responseText);
+
+            }
+        });
+    });
+
 
     var registerUserButton = $("#UserRegistrationModal button[name = 'register']").click(onUserRegisterClick);
 
@@ -62,6 +92,7 @@
 
                     $("#UserRegistrationModal").html(data);
                     var registerUserButton = $("#UserRegistrationModal button[name = 'register']").click(onUserRegisterClick);
+                    $("#UserRegistrationModal input[name = 'AcceptUserAgreement']").click(onAcceptUserAgreementClick);
 
                     $("#UserRegistrationForm").removeData("validator");
                     $("#UserRegistrationForm").removeData("unobtrusiveValidation");
@@ -73,6 +104,9 @@
 
             },
             error: function (xhr, ajaxOptions, thrownError) {
+                var errorText = "Status: " + xhr.status + " - " + xhr.statusText;
+
+                PresentClosableBootstrapAlert("#alert_placeholder_register", "danger", "Error!", errorText);
 
                 console.error(thrownError + '\r\n' + xhr.statusText + '\r\n' + xhr.responseText);
             }
