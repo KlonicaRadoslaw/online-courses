ec959b7f1b0108b68d68cb53932e1526d0a57c18
Kamilek <radek71k@onet.pl>
2024-08-30 12:42:41 +0200

    UserAuthController created

---
+++ OnlineCourses/Controllers/UserAuthController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OnlineCourses.Entities;
+using OnlineCourses.Models;
+
+namespace OnlineCourses.Controllers
+{
+    public class UserAuthController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public UserAuthController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel loginModel)
+        {
+            loginModel.LoginInValid = "true";
+
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(loginModel.Email,
+                                                                      loginModel.Password,
+                                                                      loginModel.RememberMe,
+                                                                      lockoutOnFailure: false);
+                if (result.Succeeded)
+                    loginModel.LoginInValid = "";
+                else
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt");
+            }
+            return PartialView("_UserLoginPartial", loginModel);
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout(string returnUrl = null)
+        {
+            await _signInManager.SignOutAsync();
+
+            if (returnUrl != null)
+                return LocalRedirect(returnUrl);
+            else
+                return RedirectToAction("Index", "Home");
+        }
+    }
+}
