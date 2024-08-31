9059b4cc20c6e11bea9e675ba0bf26957b7a7e8c
Kamilek <radek71k@onet.pl>
2024-08-31 13:43:16 +0200

    UsesToCategory views setup

--- OnlineCourses/Entities/Category.cs
+++ OnlineCourses/Entities/Category.cs
@@ -1,9 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using OnlineCourses.Interfaces;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OnlineCourses.Entities
 {
-    public class Category
+    public class Category : IPrimaryProperties
     {
         public int Id { get; set; }
 
