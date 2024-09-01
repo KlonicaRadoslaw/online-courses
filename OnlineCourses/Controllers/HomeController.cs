c5878eff21d521f7133c0071bc57ee3e3028842e
Kamilek <radek71k@onet.pl>
2024-09-01 11:53:19 +0200

    Get and display courses to user

--- OnlineCourses/Controllers/HomeController.cs
+++ OnlineCourses/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OnlineCourses.Entities;
+using OnlineCourses.Interfaces;
 using OnlineCourses.Models;
 using System.Diagnostics;
 
@@ -7,15 +10,42 @@ namespace OnlineCourses.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IHomeRepository _homeRepository;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,
+                              IHomeRepository homeRepository,
+                              UserManager<ApplicationUser> userManager,
+                              SignInManager<ApplicationUser> signInManager)
         {
             _logger = logger;
+            _homeRepository = homeRepository;
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            IEnumerable<CategoryItemDetailsModel> categoryItemDetailsModels = null;
+            IEnumerable<GroupedCategoryItemsByCategoryModel> groupedCategoryItemsByCategoryModels = null;
+
+            var categoryDetailsModel = new CategoryDetailsModel();
+
+            if (_signInManager.IsSignedIn(User))
+            {
+                var user = await _userManager.GetUserAsync(User);
+
+                if (user != null)
+                {
+                    categoryItemDetailsModels = await _homeRepository.GetCategoryItemDetailsForUser(user.Id);
+                    groupedCategoryItemsByCategoryModels = _homeRepository.GetGroupedCategoryItemsByCategory(categoryItemDetailsModels);
+
+                    categoryDetailsModel.GroupedCategoryItemsByCategoryModels = groupedCategoryItemsByCategoryModels;
+                }
+            }
+
+            return View(categoryDetailsModel);
         }
 
         public IActionResult Privacy()
