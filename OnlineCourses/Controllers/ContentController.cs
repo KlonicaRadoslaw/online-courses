4b6fa2702a697cafa53dcd5b1435207d9bafe42d
Kamilek <radek71k@onet.pl>
2024-09-02 15:38:20 +0200

    ContentController setup

---
+++ OnlineCourses/Controllers/ContentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineCourses.Interfaces;
+
+namespace OnlineCourses.Controllers
+{
+    public class ContentController : Controller
+    {
+        public readonly IContentRepository _contentRepository;
+        public ContentController(IContentRepository contentRepository)
+        {
+            _contentRepository = contentRepository;
+        }
+
+        public async Task<IActionResult> Index(int categoryItemId)
+        {
+            var content = await _contentRepository.GetContentByCategoryItemId(categoryItemId);
+
+            return View(content);
+        }
+    }
+}
